443364e621055e279a4d4c1a85efbbcc60b0d668
KenP <kayenp45@gmail.com>
2025-08-06 15:14:47 -0700

    Class 15: Bachelor code #1

--- Class 15/bachelor-code/1/js/main.js
+++ Class 15/bachelor-code/1/js/main.js
@@ -1,6 +1,19 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+const claire = document.querySelector("#claire");
+const sharleen = document.querySelector("#sharleen");
+
+document.querySelector('#finalRose').addEventListener('click', hide)
 
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	let opacity = getComputedStyle(claire).getPropertyValue("opacity");
+
+	let intervalID = setInterval(fade, 75);
+
+	function fade() {
+			claire.style.opacity = `${opacity - 0.1}`;
+			sharleen.style.opacity = `${opacity - 0.1}`;
+			opacity = getComputedStyle(claire).getPropertyValue("opacity");
+		};
+	if (opacity === 0) {
+		clearInterval(intervalID);
+	};
 }
