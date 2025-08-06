a3e3338497144e659c0404b73a7af8736ffdd702
KenP <kayenp45@gmail.com>
2025-08-06 11:23:05 -0700

    Class 13 hw: Javascript.info Arrow Functions

---
+++ Class 13/Javascript.info Arrow Function Basics/javascript/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+Rewrite with arrow functions
+*/
+
+//Replace Function Expressions with arrow functions in the code below:
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+
+//Replaced
+let ask = (question, yes, no) => (confirm(question)) ? yes() : no()
+
+ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+)
