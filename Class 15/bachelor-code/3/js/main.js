e9716b9f14a0dbb9aa19c9c406d6b042472135bf
KenP <kayenp45@gmail.com>
2025-08-08 11:39:35 -0700

    Class 15 hw: Bachelor code #3

--- Class 15/bachelor-code/3/js/main.js
+++ Class 15/bachelor-code/3/js/main.js
@@ -1,10 +1,40 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+const bodyElem = document.querySelector("body");
+const contestants = document.querySelectorAll(".contestant");
+const scriptElem = document.querySelector("script");
+const newH2Elem = document.createElement("h2");
+const newSpan = document.createElement("span");
+const newDocFrag = document.createDocumentFragment();
+bodyElem.insertBefore(newH2Elem, scriptElem);
+newDocFrag.appendChild(newSpan);
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose));
 
-function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+matchName();
+
+function matchName() {
+	let guess = prompt("Who do you think the final contestant is?");
+
+	while(true) {
+		if (guess.toLowerCase() === "nikki") {
+			guess = "nikki"
+			document.querySelector("#nikki").classList.toggle("hidden");
+			break;
+		} else if (guess.toLowerCase() === "claire") {
+			alert("WRONG!");
+			break;
+		} else {
+			guess = prompt("Enter one of the names below");
+		}
+	}
+
+	newSpan.innerText = `Your guess was: ${guess.toUpperCase()}`;
+	newSpan.style.color = "red";
+	newH2Elem.appendChild(newDocFrag);
+}
+
+function checkForRose(click) {
+	if(click.target.classList.contains("rose")){
+		document.querySelector('#nikki').classList.removeclair("hidden");
 	}else{
 		alert("Wrong!");
 	}
