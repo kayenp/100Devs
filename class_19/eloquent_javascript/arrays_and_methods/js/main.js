cedf2acb938cbeaba4cd26e07650f0a71d0e7e33
KenP <kayenp45@gmail.com>
2025-08-15 19:37:54 -0700

    class_19_hw_eloquent_javascript_inProgress2

--- class_19/eloquent_javascript/arrays_and_methods/js/main.js
+++ class_19/eloquent_javascript/arrays_and_methods/js/main.js
@@ -19,12 +19,34 @@ console.log(sum(range(1,10)));
 
 //As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements should go up by increments of one, corresponding to the old behavior. 
 // The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure this also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
-const rangeBetter = (start,end,step = 0) => {
-    let difference = (start < end) ? end - start : start - end;
-    let newArr = [];
-    for (let i = 0; i <= difference; i++) {
-        newArr.push((i + 1))
-    };
-    return newArr;
+/*
+GOAL - adds all numbers to array from start to end. adds array values in increments set by step value. needs to add values in either direction. needs to take into account step value for incrementing.
+*/
+//==================================================
+
+//===================================================
+
+/*
+Reversing an array
+*/
+//Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. 
+// The first, reverseArray, should take an array as its argument and produce a new array that has the same elements in the inverse order. 
+//==================================================
+
+//===================================================
+
+// The second, reverseArrayInPlace, should do what the reverse method does: modify the array given as its argument by reversing its elements. 
+const reverseArrayInPlace = arr => {
+    let val = arr.length;
+    console.log(val);
+    for (let i = 0; i < (val - 1) ; i++){
+        arr.splice(0,0,arr[i+1]);
+        arr.splice([i+2], 1);
+    }
+    return (arr);
 }
-console.log(rangeBetter(5,2));
+let arrVal = [1,2,3,4,5];
+console.log(reverseArrayInPlace(arrVal));
+
+// Neither may use the standard reverse method.
+//Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
