3bb52115af1418dbd3886d6b8ce3c657a2c6a28d
KenP <kayenp45@gmail.com>
2025-08-16 08:19:58 -0700

    class_19_hw_eloquent_javascript_inProgress

--- class_19/eloquent_javascript/arrays_and_methods/js/main.js
+++ class_19/eloquent_javascript/arrays_and_methods/js/main.js
@@ -11,20 +11,26 @@ const range = (start,end) => {
     };
     return newArr;
 }
-console.log(range(32,50));
+
 
 //Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
 const sum = (arr) => arr.reduce((acc,currVal) => acc + currVal);
-console.log(sum(range(1,10)));
+
 
 //As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements should go up by increments of one, corresponding to the old behavior. 
 // The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure this also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 /*
 GOAL - adds all numbers to array from start to end. adds array values in increments set by step value. needs to add values in either direction. needs to take into account step value for incrementing.
 */
-//==================================================
-
-//===================================================
+const rangeBetter = (start,end,step = 0) => {
+    let newArr = [];
+    let difference = Math.abs(start - end);
+    step = Math.abs(step);
+    for (let i = 0; i <= difference; (step === 0) ? i++ : i = (i + step)) {
+        (start < end) ? newArr.push(start + i) : newArr.push(start - i);
+    };
+    return newArr;
+}
 
 /*
 Reversing an array
@@ -32,8 +38,15 @@ Reversing an array
 //Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. 
 // The first, reverseArray, should take an array as its argument and produce a new array that has the same elements in the inverse order. 
 //==================================================
+let arrVal = [1,2,3,4,5];
 
-//===================================================
+const reverseArray = arr => {
+    let newArr = [];
+    for (let i = (arr.length - 1); i >= 0; i--) {
+        newArr.push(arr[i]);
+    };
+    return newArr;
+}
 
 // The second, reverseArrayInPlace, should do what the reverse method does: modify the array given as its argument by reversing its elements. 
 const reverseArrayInPlace = arr => {
@@ -45,8 +58,9 @@ const reverseArrayInPlace = arr => {
     }
     return (arr);
 }
-let arrVal = [1,2,3,4,5];
-console.log(reverseArrayInPlace(arrVal));
 
 // Neither may use the standard reverse method.
 //Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+//A: reverseArray should run faster and be more flexible/useful than reverseArrayInPlace
+
+
