7e3bab83ea0ae0e1ee8b4129273fb0f3e7c96aac
Ken <kayenp45@gmail.com>
2025-09-09 16:03:41 -0700

    class_22_hw_extras_javascript.info_objects_4_object_methods_this

--- class_22/extras/javascript.info_objects/4_Object_methods_this/js/main.js
+++ class_22/extras/javascript.info_objects/4_Object_methods_this/js/main.js
@@ -24,9 +24,8 @@ Create a calculator
 //read() prompts for two values and saves them as object properties with names a and b respectively.
 //sum() returns the sum of saved values.
 //mul() multiplies saved values and returns the result.
-
 let calculator = {
-  read(a,b){
+  read(){
     this.a = Number(prompt("Enter A"));
     this.b = Number(prompt("Enter B"));
   },
@@ -40,3 +39,46 @@ let calculator = {
 //calculator.read();
 //alert(calculator.sum());
 //alert(calculator.mul());
+
+/*
+Chaining
+*/
+//There’s a ladder object that allows you to go up and down:
+/*
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+  }
+};
+*/
+//Now, if we need to make several calls in sequence, we can do it like this:
+//ladder.up();
+//ladder.up();
+//ladder.down();
+//ladder.showStep(); // 1
+//ladder.down();
+//ladder.showStep(); // 0
+//Modify the code of up, down, and showStep to make the calls chainable, like this:
+//ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
+let ladder = {
+  step: 0,
+  up: function() {
+    this.step++;
+	return this;
+  },
+  down: function() {
+    this.step--;
+	return this;
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+	return this;
+  }
+};
