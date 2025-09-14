f64b67c556ef573abfc93bea9b94641693f16d96
Ken <kayenp45@gmail.com>
2025-09-14 16:25:03 -0700

    class_28_hw_codeAcademy_introToJavascript_complete

--- Class 15/template/js/main.js
+++ Class 15/template/js/main.js
@@ -1,10 +1,42 @@
 "use strict";
 
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  });
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const { functionality } = robot;
+
+console.log(functionality);
