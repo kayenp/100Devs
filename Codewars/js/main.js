5e00172c5ffbf204f3586f08d0f93eea1960149a
Ken <githubdotcom20250603_1349@merchnt.online>
2025-10-30 08:28:48 -0700

    Daily Codewars 2025_10_30 #5

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4149,3 +4149,10 @@ function numObj(s){
   });
   return arrOfObj;
 }
+
+//#5
+//The Office III = Broken Photocopier
+//https://www.codewars.com/kata/57ed56657b45ef922300002b
+function broken(x){
+  return x.split('').map(ele => (ele == 1) ? 0 : 1).join('');
+}
