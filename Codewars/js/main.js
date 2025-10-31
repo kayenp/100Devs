6dce853be749dd7eecae08b5ee80002d35c43a8c
Ken <githubdotcom20250603_1349@merchnt.online>
2025-10-31 10:02:51 -0700

    Daily Codewars 2025_10_31 #4

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4202,3 +4202,10 @@ function gordon(a){
           })
           .join(''));
 }
+
+//#4
+//Keep up the hoop
+//https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+function hoopCount (n) {
+   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+};
