71acbebdcc529124b8b8afc7be2e3465f47a8b99
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-01 10:58:05 -0700

    Daily Codewars 2025_11_01 #2-#3

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4234,3 +4234,18 @@ function scrollingText(text){
   }
   return arr;
 }
+
+//#2
+//Is it even?
+//https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+function testEven(n) {
+    return n % 2 == 0;
+}
+
+//#3
+//Powers of 
+//https://www.codewars.com/kata/57a083a57cb1f31db7000028
+function powersOfTwo(n){
+  return Array.from({ length: n+1 }, ((v, k) => 2**k));
+}
+
