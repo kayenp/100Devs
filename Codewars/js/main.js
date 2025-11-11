10e8dc80aadc6c8c7d87b07440c633e7b20f9e13
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-10 16:56:05 -0800

    Daily Codewars 2025_11_10 #4

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4723,3 +4723,13 @@ function take(arr, n) {
   return arr.slice(0, n);
 }
 
+//#4
+//101 Dalmations-squash the bugs, not the dogs!
+//https://www.codewars.com/kata/56f6919a6b88de18ff000b36
+function howManyDalmatians(number) {
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  var respond = (number <= 10) ? dogs[0] 
+                 : (number <= 50) ? dogs[1] 
+                 : (number == 101) ? dogs[3] : dogs[2]
+  return respond
+}
