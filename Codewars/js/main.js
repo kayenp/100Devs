2b40e1d1608752916f693be9dcaea26dacc7c302
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-10 16:45:26 -0800

    Daily Codewars 2025_11_10 #3

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4717,5 +4717,9 @@ function chromosomeCheck(sperm) {
 }
 
 //#3
-//
-//
+//Enumerable Magic #25 - Take the First N Elements
+//https://www.codewars.com/kata/545afd0761aa4c3055001386
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+
