0471ff9f7357012a2736ff05db6a2e78602a6c52
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-11 10:12:53 -0800

    Daily Codewars 2025_11_11 #2

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4757,3 +4757,13 @@ function cake(x, y) {
   return (candlesFallen/x > .7) ? 'Fire!' : 'That was close!';
 }
 
+//#2
+//Fix your code before the garden dies!
+//https://www.codewars.com/kata/57158fb92ad763bb180004e7
+function rainAmount(mm){
+    if (mm < 40) {
+         return "You need to give your plant " + `${40 - mm}` + "mm of water"
+    } else {
+         return "Your plant has had more than enough water for today!"
+    };
+}
