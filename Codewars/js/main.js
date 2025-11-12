fa059abcf0c05f658f470b2c7a41fe66d757c85b
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-12 08:46:47 -0800

    Daily Codewars 2025_11_12 #2

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4798,5 +4798,40 @@ function include(arr, item){
 //Array Mash
 //https://www.codewars.com/kata/582642b1083e12521f0000da
 function arrayMash (array1, array2) {
-  return [...Array(array1.length)].map((ele, ind, arr) => [array1[ind], array2[ind]]).flat();
+  return [...Array(array1.length)].map((_, ind) => [array1[ind], array2[ind]]).flat();
 }
+
+//#2
+//Training JS #3: Basic data types--String
+//https://www.codewars.com/kata/571edea4b625edcb51000d8e
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
+//#3
+//Fundamentals: Return
+//https://www.codewars.com/kata/55a5befdf16499bffb00007b
