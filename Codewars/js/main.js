3dc21fc53ea3e63e92f0e0b9e246f6aa5015fbd0
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-12 08:48:06 -0800

    Daily Codewars 2025_11_12 #3

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -4835,3 +4835,26 @@ function answer3(){
 //#3
 //Fundamentals: Return
 //https://www.codewars.com/kata/55a5befdf16499bffb00007b
+function add(a,b){
+    return a + b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function multiply(a,b){
+    return  a * b;
+}
+
+function mod(a,b){
+    return a % b;
+}
+   
+function exponent(a,b){
+    return a**b;
+}
+    
+function subt(a,b){
+    return a - b;
+}
