5daead83dcdbc62be1a3a63a00864084a1ab3cb3
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-16 10:16:40 -0800

    Daily Codewars 2025_11_16 #5

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -5102,3 +5102,12 @@ function howManydays(month){
   }
   return days;
 }
+
+//#5
+//Grasshopper - Messi Goals
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5
+
+var totalGoals = (() => laLigaGoals + championsLeagueGoals + copaDelReyGoals)();
