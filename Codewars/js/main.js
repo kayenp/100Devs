6830d2adb432dd0cbb3b1f364be0f823ab9e82a9
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-23 12:06:21 -0800

    Daily Codewars 2025_11_23 #5

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -5543,3 +5543,19 @@ var Person = function(){
   }
   return person;
 }
+
+//#5
+//A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+//https://www.codewars.com/kata/562e98755e9214cd2500003d
+function yourFutureCareer() {
+	let career = Math.random();
+		if (career <= 0.32) {
+			return 'FrontEnd Developer';
+		 } else if (career <= 0.65) {
+			return 'BackEnd Developer';
+		} else {
+			return 'Full-Stack Developer';
+		}
+}
+
+yourFutureCareer();
