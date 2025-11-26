3d0693ee08bdcb70bfa769e1d2ed4f23946cb430
Ken <githubdotcom20250603_1349@merchnt.online>
2025-11-26 15:10:30 -0800

    Daily Codewars 2025_11_26 #1

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -5696,3 +5696,15 @@ function whatNumberIsIt(n){
 function colorOf(...rgb) {
   return `#${rgb.map(ele => (ele.toString(16).length < 2) ? '0' + ele.toString(16) : ele.toString(16)).join('')}`
 }
+
+/*
+==========
+2025_11_26
+==========
+*/
+//#1
+//Gravity Flip
+//https://www.codewars.com/kata/5f70c883e10f9e0001c89673
+const flip=(d, a)=>{
+  return (d === 'R') ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a);
+}
