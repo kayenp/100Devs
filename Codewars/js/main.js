3387cea38e011a2135862d8cdc76cbf73aeec0c9
Ken <githubdotcom20250603_1349@merchnt.online>
2026-01-22 14:03:09 -0800

    Daily Codewars #4

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -6169,5 +6169,20 @@ class Cat extends Animal {
 }
 
 //#4
-//
-//
+//Barking mad
+//https://www.codewars.com/kata/54dba07f03e88a4cec000caf
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function() {
+  return "Woof";
+};
