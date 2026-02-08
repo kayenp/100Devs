3074ebf554438a21e68407d565acbddd2de00159
Ken <githubdotcom20250603_1349@merchnt.online>
2026-02-08 14:32:28 -0800

    Daily Codewars #4

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -6336,3 +6336,29 @@ function pak(s){
   };
   return resultArr.join(' ');
 }
+
+//#4
+//Welcome!
+//https://www.codewars.com/kata/577ff15ad648a14b780000e7
+function greet(language) {
+const db = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso"
+};
+  return (db[language]) ? db[language] : 'Welcome';
+}
