2e1e6e732595224337c23f28d1f1ae620fde3518
Ken <githubdotcom20250603_1349@merchnt.online>
2026-04-06 12:10:58 -0700

    Daily Codewars #1

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -7415,3 +7415,17 @@ function sortByLanguage(list) {
       : (aName > bName) ? 1 : 0;
   })
 }
+
+ /*
+==========
+2026_04_06
+==========
+*/
+//#1
+//Quadrants
+//https://www.codewars.com/kata/643af0fa9fa6c406b47c5399
+function quadrant(x, y) {
+  return (x > 0 && y > 0) ? 1 
+    : (x < 0 && y > 0) ? 2
+    : (x < 0 && y < 0) ? 3 : 4
+}
