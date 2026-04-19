946c9d9bba629febcc2e34871ad7d9ceb16cbc7f
Ken <githubdotcom20250603_1349@merchnt.online>
2026-04-19 13:12:06 -0700

    Daily Codewars #2

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -7588,3 +7588,8 @@ function operate(pcSet, operation) {
   
   return output.sort((a, b) => a - b);
 }
+
+//#2
+//Vowel Count
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3
+const getCount = str => str.length - str.replaceAll(/[aeiou]/g, '').length
