543cdb050336641059c4a09c6d8b93535f0d9a24
Ken <githubdotcom20250603_1349@merchnt.online>
2026-04-23 12:29:32 -0700

    Daily Codewars #1

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -7665,3 +7665,19 @@ function solve(arr){
 //#1
 //Find Duplicates
 //https://www.codewars.com/kata/5558cc216a7a231ac9000022
+function duplicates(arr) {
+  const dupArr = [];
+  
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i-1; j >= 0; j--) {
+      if (arr[i] == arr[j] && typeof arr[i] == typeof arr[j]) {
+        if (dupArr.includes(arr[i])) {
+          break;
+        } else {
+          dupArr.push(arr[i])
+        }
+      }
+    }
+  }
+  return dupArr;
+}
