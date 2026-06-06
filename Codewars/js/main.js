7c4f961a596b846bbc09e5aa726ebf4ad064030d
Ken <githubdotcom20250603_1349@merchnt.online>
2026-06-06 13:20:10 -0700

    Daily Codewars #5

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -8784,3 +8784,17 @@ function isIsogram(str){
   return (setStr.length === str.length);
 }
 
+//#5
+//Jaden Casing Strings
+//https://www.codewars.com/kata/5390bac347d09b7da40006f6
+Object.defineProperty(
+  String.prototype,
+  'toJadenCase',
+  { value :
+   function toJadenCase() {
+     return this.split(' ')
+       .map(word => word[0].toUpperCase() + word.slice(1))
+       .join(' ');
+   }
+  }
+);
