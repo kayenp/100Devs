65955ea662aadb84ffdb7997b30916fceaf53a26
Ken <githubdotcom20250603_1349@merchnt.online>
2026-07-24 14:42:46 -0700

    Daily Codeewars #2

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -10324,3 +10324,12 @@ function gimmeTheLetters(sp) {
     return output.toUpperCase();
   }
 }
+
+//#2
+//"this" is a problem
+//https://www.codewars.com/kata/547c71fdc5b2b38db1000098
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.name = this.firstName + ' ' + this.lastName;
+}
