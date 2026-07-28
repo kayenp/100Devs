309f21bb2a7816ca9b40280e68d8d872759f4147
Ken <githubdotcom20250603_1349@merchnt.online>
2026-07-27 20:00:50 -0700

    Daily Codewars #5

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -10487,3 +10487,22 @@ function whoseMove(lastPlayer, win) {
     : (lastPlayer === "white") ? "black"
     : (win) ? "black" : "white";
 };
+
+//#5
+//Broken Counter
+//https://www.codewars.com/kata/526471539d52735c620000c6
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
