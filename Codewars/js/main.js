f0954092e33ab3bfcdeea9dc6892a3ead7b7abec
Ken <githubdotcom20250603_1349@merchnt.online>
2026-07-29 20:12:38 -0700

    Daily Codewars #3

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -10591,3 +10591,21 @@ const greetBen = () => {
   name = 'Ben';
   return 'Hello, ' + name + '!'
 }
+
+//#3
+//Max Headroom and JavaScript style
+//https://www.codewars.com/kata/52a47dd7e950edabfa000391
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  return getMax1()
+}
+
