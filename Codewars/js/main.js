8eacda31352e7972060de58b4be9d76853941dec
Ken <githubdotcom20250603_1349@merchnt.online>
2026-08-06 13:48:40 -0700

    Daily Codewars #1

--- Codewars/js/main.js
+++ Codewars/js/main.js
@@ -10874,3 +10874,19 @@ function histogram(results) {
   }
   return output.join("");
 }
+
+/*
+==========
+2026_08_06
+==========
+*/
+//#1
+//Data Reverse
+//https://www.codewars.com/kata/569d488d61b812a0f7000015
+function dataReverse(data) {
+  const output = [];
+  for (let i = 0; i < data.length; i+=8) {
+    output.push(data.slice(i,i+8));
+  }; 
+  return output.reverse().flat();
+}
